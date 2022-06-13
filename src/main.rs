28d899a8ec994dbea1244f786c2af9f025532c11
Ryan Fox <rfox@kepler.space>
2022-06-12 20:20:58 -0400

    Spawn new enemies on a timer.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,12 @@ fn main() {
 
 pub struct GamePlugin;
 
+#[derive(Component)]
+struct EnemySpawnConfig {
+    timer: Timer,
+    desired_amount: usize,
+}
+
 // Player component
 #[derive(Component)]
 struct Player;
@@ -31,12 +37,18 @@ struct PreventOverlap;
 struct Speed(f32, f32);
 
 #[derive(Component)]
-struct Damage(i32);
+struct Health {
+    max: u32,
+    current: u32,
+}
+
+#[derive(Component)]
+struct Damage(u32);
 
 #[derive(Component)]
 struct ShootBullet {
     cooldown: Timer,
-    damage: i32,
+    damage: u32,
     size: f32,
     speed: f32,
 }
@@ -48,6 +60,51 @@ struct BulletBundle {
 
     #[bundle]
     sprite: SpriteBundle,
+}
+
+fn spawn_enemies(mut commands: Commands, num: usize) {
+    let mut rng = rand::thread_rng();
+    for _ in 1..num {
+        commands
+            .spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color: ENEMY_COLOR,
+                    ..default()
+                },
+                transform: Transform {
+                    translation: Vec3::new(
+                        rng.gen_range(-600.0..600.0),
+                        rng.gen_range(-400.0..400.0),
+                        1.0,
+                    ),
+                    scale: Vec3::new(10.0, 10.0, 1.0),
+                    ..default()
+                },
+                ..default()
+            })
+            .insert(Health { max: 1, current: 1 })
+            .insert(Speed(0.0, 0.0))
+            .insert(PreventOverlap)
+            .insert(Enemy);
+    }
+}
+
+fn spawn_new_enemies(
+    commands: Commands,
+    time: Res<Time>,
+    mut spawn: ResMut<EnemySpawnConfig>,
+    enemies: Query<&Enemy>,
+) {
+    spawn.timer.tick(time.delta());
+
+    if spawn.timer.finished() {
+        let num_enemies = enemies.iter().len();
+        if spawn.desired_amount > num_enemies {
+            let amount = spawn.desired_amount - num_enemies;
+
+            spawn_enemies(commands, amount.clamp(0, 30));
+        }
+    }
 }
 
 // spawn player system
@@ -78,29 +135,7 @@ fn setup(mut commands: Commands) {
         .insert(PreventOverlap)
         .insert(Player);
 
-    let mut rng = rand::thread_rng();
-    for _ in 1..100 {
-        commands
-            .spawn_bundle(SpriteBundle {
-                sprite: Sprite {
-                    color: ENEMY_COLOR,
-                    ..default()
-                },
-                transform: Transform {
-                    translation: Vec3::new(
-                        rng.gen_range(-600.0..600.0),
-                        rng.gen_range(-400.0..400.0),
-                        1.0,
-                    ),
-                    scale: Vec3::new(10.0, 10.0, 1.0),
-                    ..default()
-                },
-                ..default()
-            })
-            .insert(Speed(0.0, 0.0))
-            .insert(PreventOverlap)
-            .insert(Enemy);
-    }
+    //spawn_enemies(commands, 100);
 }
 
 fn handle_input(
@@ -131,8 +166,8 @@ fn enemy_ai(
     for (mut speed, transform) in query.iter_mut() {
         let dx = player.translation.x - transform.translation.x;
         let dy = player.translation.y - transform.translation.y;
-        speed.0 = dx.clamp(-1.0, 1.0);
-        speed.1 = dy.clamp(-1.0, 1.0);
+        speed.0 = dx.clamp(-0.5, 0.5);
+        speed.1 = dy.clamp(-0.5, 0.5);
     }
 }
 
@@ -171,6 +206,39 @@ fn check_collisions(
                     Collision::Bottom => speed.1 = if speed.1 > 0.0 { 0.0 } else { speed.1 },
                     _ => {}
                 }
+            }
+        }
+    }
+}
+
+fn check_bullet_collisions(
+    mut commands: Commands,
+    bullets: Query<(Entity, &Damage, &Transform)>,
+    mut targets: Query<(Entity, &mut Health, &Transform)>,
+) {
+    for (bullet, bullet_damage, bullet_transform) in bullets.iter() {
+        for (target, mut health, target_transform) in targets.iter_mut() {
+            if bullet == target {
+                continue;
+            }
+            let dist = bullet_transform
+                .translation
+                .distance_squared(target_transform.translation);
+            if dist > 400.0 {
+                continue;
+            }
+            let collision = collide(
+                bullet_transform.translation,
+                bullet_transform.scale.truncate(),
+                target_transform.translation,
+                target_transform.scale.truncate(),
+            );
+            if let Some(_collision) = collision {
+                health.current -= bullet_damage.0;
+                if health.current == 0 {
+                    commands.entity(target).despawn();
+                }
+                commands.entity(bullet).despawn();
             }
         }
     }
@@ -230,12 +298,18 @@ impl Plugin for GamePlugin {
             ..Default::default()
         })
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .insert_resource(EnemySpawnConfig {
+            timer: Timer::new(std::time::Duration::from_secs(5), true),
+            desired_amount: 150,
+        })
         .add_startup_system(setup)
         .add_system(enemy_ai)
         .add_system(check_collisions.after(enemy_ai))
         .add_system(move_things.after(check_collisions))
         .add_system(handle_input.before(move_things))
         .add_system(shoot_bullet.before(move_things))
+        .add_system(check_bullet_collisions.before(move_things))
+        .add_system(spawn_new_enemies)
         .add_system(bevy::input::system::exit_on_esc_system);
     }
 }
